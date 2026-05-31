83e3e447f959f97bfe1cb45d7b3ea3803d7c3e46
Person885 <keyboaqwerty@gmail.com>
2026-05-31 13:04:42 -0600

    Update lang-en.js

--- js/languages/lang-en.js
+++ js/languages/lang-en.js
@@ -1487,6 +1487,7 @@ LANGUAGES.EN = {
         'auto-mining_ascend-name': `Auto-Mining Ascension`,
         'auto-nucleobase-name': `Auto-Nucleobases`,
         'auto-gal_eu-name': `Auto-Galactic Exploration Upgrade`,
+        'auto-hadron_research-name': `Auto-Hadronize Research`,
 
         'auto-cost': (D,cost,name) => `Decrease Interval by ${formatReduction(D,0)}.<br>Cost: ${format(cost,0)} ${name}`,
         'auto-interval': (a,b,maxed) => `Interval: ${format(a,3)}s`+(maxed ? "" :` ➜ ${format(b,3)}s`),
